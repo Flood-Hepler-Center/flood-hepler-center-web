f802d944021db952bcd8a32a386dd3ba3ae83e61
tanapon.m <tanapon.m@rentspree.com>
2024-11-28 23:39:40 +0700

    add logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { LogoIcon } from '@/components/icons';
 import { title, subtitle } from '@/components/primitives';
 import DefaultLayout from '@/layouts/default';
 
@@ -6,6 +7,12 @@ export default function IndexPage() {
     <DefaultLayout>
       <section className='flex flex-col items-center justify-center gap-4 py-8 md:py-10'>
         <div className='inline-block max-w-xl text-center justify-center'>
+          <div className='flex justify-center w-full md:hidden mb-2'>
+            <LogoIcon width={150} height={50} />
+          </div>
+          <div className='hidden md:flex md:justify-center w-full mb-3 '>
+            <LogoIcon width={200} height={80} />
+          </div>
           <span className={title()}>ศูนย์&nbsp;</span>
           <span className={title({ color: 'pink' })}>ช่วยเหลือ&nbsp;</span>
           <br />
